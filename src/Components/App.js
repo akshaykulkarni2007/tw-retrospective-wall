4e83601ea88bcb57c820f5f92e665de22a67c654
Akshay Kulkarni <akshaykulkarni2007@gmail.com>
2018-09-13 19:56:10 +0530

    initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react"
+
+import "./App.css"
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<nav class="navbar navbar-dark bg-dark">
+					<a class="navbar-brand" href="#">
+						TW Retrospective Wall
+					</a>
+				</nav>
+
+				<h1 className="text-center py-5">TW Retrospective Wall</h1>
+			</div>
+		)
+	}
+}
+
+export default App
